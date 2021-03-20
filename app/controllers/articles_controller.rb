79a05075f4b4c195cfea8371201d45bf40cc25de
Onur Ozer <onurozer@gmail.com>
2021-03-20 22:47:13 +0100

    saving articles securely

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,11 +12,19 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title: '...', body: '...')
+    @article = Article.new(article_params)
     if @article.save
       redirect_to @article
     else
       render :new
     end
+  end
+
+  private
+
+  # See https://guides.rubyonrails.org/getting_started.html#using-strong-parameters
+  # for more info on the rationale for cleaning parameters before submitting to models
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end

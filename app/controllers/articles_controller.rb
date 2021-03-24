f1ec37f8cc3e2cd3113c7b6daf5eebb8f8c2116c
Onur Ozer <onurozer@gmail.com>
2021-03-24 09:02:50 +0100

    simplest possible authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: 'admin', password: 'admin', except: %i[index show]
   def index
     @articles = Article.all
   end
